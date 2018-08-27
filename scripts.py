0dff5d2f4a18a6b87fad3dfa3bd8978bf571d51f
Eric Ma <ericmajinglong@gmail.com>
2018-08-26 21:04:07 -0400

    Created script that rebuilds QR codes.

---
+++ scripts.py
@@ -0,0 +1,34 @@
+import click
+import pyqrcode as pq
+from pyqrcode.qrspecial import QrMeCard
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def slidesqr():
+    url = 'https://ericmjl.github.io/curve-fitting-talk/'
+    pq.create(url).png('images/talk.png', scale=10)
+
+
+@main.command()
+def contactqr():
+    name = 'Eric J. Ma'
+    url = 'https://ericmjl.github.io'
+    note = 'DIGILITY 2018, speaker, Bayesian.'
+
+    mecard = QrMeCard(name=name, url=url, memo=note)
+    pq.create(str(mecard)).png('images/ericmjl.png', scale=10)
+
+
+@main.command()
+def execute_all():
+    slidesqr()
+    contactqr()
+
+
+if __name__ == '__main__':
+    main()
